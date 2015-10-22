3188227aa1ee0b327d71e3081c5b33ce435c5a88
tripplyons <tripplyons@gmail.com>
2015-10-22 16:57:17 -0700

    Got it working!!! :)

--- ai/simple.js
+++ ai/simple.js
@@ -1,15 +1,17 @@
 function canMoveDown(grid) {
 	var retrn = false;
 	grid.eachCell(function(x, y, content) {
-		if (y < 3) {
-			if(grid.cellOccupied({x:x, y:y})) {
-				if(!grid.cellOccupied({x:x, y:y+1})) {
-					retrn = true;
+		if (y != grid.size-1) {
+			try {
+				if(grid.cellOccupied({x:x, y:y})) {
+					if(!grid.cellOccupied({x:x, y:y+1})) {
+						retrn = true;
+					}
+					if(content.value == grid.cellContent({x:x, y:y+1}).value) {
+						retrn = true;
+					}
 				}
-				if(content == grid.cellContent({x:x, y:y+1})) {
-					retrn = true;
-				}
-			}
+			} catch(err) {  }
 		}
 	});
 	
@@ -19,15 +21,17 @@ function canMoveDown(grid) {
 function canMoveLeft(grid) {
 	var retrn = false;
 	grid.eachCell(function(x, y, content) {
-		if (x > 0) {
-			if(grid.cellOccupied({x:x, y:y})) {
-				if(!grid.cellOccupied({x:x-1, y:y})) {
-					retrn = true
+		if (x != 0) {
+			try {
+				if(grid.cellOccupied({x:x, y:y})) {
+					if(!grid.cellOccupied({x:x-1, y:y})) {
+						retrn = true
+					}
+					if(content.value == grid.cellContent({x:x-1, y:y}).value) {
+						retrn = true;
+					}
 				}
-				if(content == grid.cellContent({x:x-1, y:y})) {
-					retrn = true;
-				}
-			}
+			} catch(err) {  }
 		}
 	});
 	
@@ -37,15 +41,17 @@ function canMoveLeft(grid) {
 function canMoveRight(grid) {
 	var retrn = false;
 	grid.eachCell(function(x, y, content) {
-		if (x < 3) {
-			if(grid.cellOccupied({x:x, y:y})) {
-				if(!grid.cellOccupied({x:x+1, y:y})) {
-					retrn = true;
+		if (x != grid.size-1) {
+			try {
+				if(grid.cellOccupied({x:x, y:y})) {
+					if(!grid.cellOccupied({x:x+1, y:y})) {
+						retrn = true;
+					}
+					if(content.value == grid.cellContent({x:x+1, y:y}).value) {
+						retrn = true;
+					}
 				}
-				if(content == grid.cellContent({x:x+1, y:y})) {
-					retrn = true;
-				}
-			}
+			} catch(err) {  }
 		}
 	});
 	
@@ -54,17 +60,32 @@ function canMoveRight(grid) {
 
 function AI(grid) {
 	console.log(grid);
+	var bottomRowFull = true;
+	for(var col=0; col<grid.size; col++) {
+		bottomRowFull = bottomRowFull && grid.cellOccupied({x:col, y:grid.size-1});
+	}
+	var leftColFull = true;
+	for(var row=0; row<grid.size; row++) {
+		leftColFull = leftColFull && grid.cellOccupied({x:0, y:row});
+	}
+	
+	if(!bottomRowFull) {
+		if (canMoveRight(grid)) {
+			return 1;
+		} else if (canMoveLeft(grid)) {
+			return 3;
+		} else if (canMoveDown(grid)) {
+			return 2;
+		}
+	}
+	
 	if (canMoveDown(grid)) {
-		console.log("DOWN");
 		return 2;
 	} else if (canMoveLeft(grid)) {
-		console.log("LEFT");
 		return 3;
 	} else if (canMoveRight(grid)) {
-		console.log("RIGHT");
 		return 1;
 	}
-
-	console.log("UP");
+	
 	return 0;
 }
